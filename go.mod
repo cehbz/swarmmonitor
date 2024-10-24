4c53b2f5fc03364fd9f72887cddcbc558e3af7c0
Charles Haynes <ceh+github@ceh.bz>
2024-10-24 11:46:26 +0200

    switch from influx to sqlite

--- go.mod
+++ go.mod
@@ -2,12 +2,46 @@ module github.com/cehbz/swarmmonitor
 
 go 1.23.2
 
-require github.com/influxdata/influxdb-client-go/v2 v2.14.0
+require (
+	github.com/cehbz/qbittorrent v1.5.0
+	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.19.0
+	modernc.org/sqlite v1.33.1
+)
 
 require (
-	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
-	github.com/google/uuid v1.3.1 // indirect
-	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
-	github.com/oapi-codegen/runtime v1.0.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
+	modernc.org/libc v1.55.3 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- swarmmonitor.go
+++ swarmmonitor.go
@@ -1,333 +1,437 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"encoding/json"
-	"flag"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
-	"net/http"
-	"net/url"
 	"os"
-	"sync"
+	"os/signal"
+	"strings"
+	"syscall"
 	"time"
 
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/cehbz/qbittorrent"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	_ "modernc.org/sqlite" // instead of github.com/mattn/go-sqlite3
 )
 
-type Config struct {
-	QBitHost     string
-	QBitUsername string
-	QBitPassword string
-	InfluxHost   string
-	InfluxToken  string
-	InfluxOrg    string
-	InfluxBucket string
-	SocketPath   string
+type TorrentNotification struct {
+	Hash string
+	ID   int
 }
 
-type TorrentInfo struct {
-	Name     string  `json:"name"`
-	Hash     string  `json:"hash"`
-	Size     int64   `json:"size"`
-	Progress float64 `json:"progress"`
-}
+var newTorrentChan chan TorrentNotification
 
-type PeerInfo struct {
-	IP          string  `json:"ip"`
-	Client      string  `json:"client"`
-	Progress    float64 `json:"progress"`
-	Downloaded  int64   `json:"downloaded"`
-	Uploaded    int64   `json:"uploaded"`
-	TimestampNS int64   `json:"timestamp"`
-	Hash        string  `json:"hash"`
-	Name        string  `json:"name"` // Torrent name
-}
+var (
+	socketPath   string = "/tmp/swarmmonitor.sock"
+	qBitAddr     string = "127.0.0.1"
+	qBitPort     string = "8080"
+	qBitUsername string = "admin"
+	qBitPassword string = "adminpass"
+	configPath   string = os.Getenv("HOME") + "/.config/swarmmonitor/config.toml"
+)
 
-type TorrentMonitor struct {
-	startTime time.Time
-	hash      string
-	name      string
-	active    bool
-	mu        sync.Mutex
-}
+var rootCmd *cobra.Command
 
-type SwarmMonitor struct {
-	config       Config
-	client       *http.Client
-	influxClient influxdb2.Client
-	cookie       string
-	monitors     map[string]*TorrentMonitor
-	mu           sync.RWMutex
-}
-
-func NewSwarmMonitor(config Config) (*SwarmMonitor, error) {
-	monitor := &SwarmMonitor{
-		config:   config,
-		client:   &http.Client{},
-		monitors: make(map[string]*TorrentMonitor),
-	}
-
-	monitor.influxClient = influxdb2.NewClient(config.InfluxHost, config.InfluxToken)
-
-	return monitor, nil
-}
-
-func (m *SwarmMonitor) login() error {
-	data := url.Values{}
-	data.Set("username", m.config.QBitUsername)
-	data.Set("password", m.config.QBitPassword)
-
-	resp, err := m.client.PostForm(fmt.Sprintf("%s/api/v2/auth/login", m.config.QBitHost), data)
+func initDB() *sql.DB {
+	var err error
+	db, err := sql.Open("sqlite", "./torrents.db")
 	if err != nil {
-		return err
+		log.Fatalf("Failed to open database: %v", err)
 	}
-	defer resp.Body.Close()
 
-	for _, cookie := range resp.Cookies() {
-		if cookie.Name == "SID" {
-			m.cookie = cookie.Value
-			return nil
-		}
+	schema := `
+	CREATE TABLE IF NOT EXISTS torrents (
+		id INTEGER PRIMARY KEY,
+		name TEXT NOT NULL,
+		hash TEXT NOT NULL UNIQUE
+	);
+
+	CREATE TABLE IF NOT EXISTS peers (
+		id INTEGER PRIMARY KEY,
+		ip TEXT NOT NULL,
+		port INTEGER NOT NULL,
+		country_code TEXT,
+		client TEXT,
+		UNIQUE(ip, port)  -- prevent duplicate peer entries
+	);
+
+	CREATE TABLE IF NOT EXISTS peer_metrics (
+		timestamp TIMESTAMP NOT NULL,
+		peer_id INTEGER NOT NULL,
+		torrent_id INTEGER NOT NULL,
+		uploaded INTEGER NOT NULL,
+		downloaded INTEGER NOT NULL,
+		pct_complete REAL NOT NULL,
+		FOREIGN KEY (peer_id) REFERENCES peers(id),
+		FOREIGN KEY (torrent_id) REFERENCES torrents(id)
+	);
+
+	CREATE TABLE IF NOT EXISTS main_metrics (
+		timestamp TIMESTAMP NOT NULL,
+		torrent_id INTEGER NOT NULL,
+		uploaded INTEGER NOT NULL,
+		downloaded INTEGER NOT NULL,
+		FOREIGN KEY (torrent_id) REFERENCES torrents(id)
+	);
+	`
+	_, err = db.Exec(schema)
+	if err != nil {
+		log.Fatalf("Failed to create tables: %v", err)
 	}
-	return fmt.Errorf("no session cookie found")
+	return db
 }
 
-func (m *SwarmMonitor) getTorrentInfo(hash string) (*TorrentInfo, error) {
-	req, err := http.NewRequest("GET",
-		fmt.Sprintf("%s/api/v2/torrents/info?hashes=%s", m.config.QBitHost, hash), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.AddCookie(&http.Cookie{Name: "SID", Value: m.cookie})
-	resp, err := m.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var torrents []TorrentInfo
-	if err := json.NewDecoder(resp.Body).Decode(&torrents); err != nil {
-		return nil, err
-	}
-
-	if len(torrents) == 0 {
-		return nil, fmt.Errorf("torrent not found")
-	}
-
-	return &torrents[0], nil
-}
-
-func (m *SwarmMonitor) getPeerInfo(hash string, name string) ([]PeerInfo, error) {
-	req, err := http.NewRequest("GET",
-		fmt.Sprintf("%s/api/v2/sync/torrent_peers/%s", m.config.QBitHost, hash), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.AddCookie(&http.Cookie{Name: "SID", Value: m.cookie})
-	resp, err := m.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var peers []PeerInfo
-	if err := json.NewDecoder(resp.Body).Decode(&peers); err != nil {
-		return nil, err
-	}
-
-	now := time.Now().UnixNano()
-	for i := range peers {
-		peers[i].TimestampNS = now
-		peers[i].Hash = hash
-		peers[i].Name = name
-	}
-
-	return peers, nil
-}
-
-func (m *SwarmMonitor) writePeerData(peers []PeerInfo) error {
-	writeAPI := m.influxClient.WriteAPIBlocking(m.config.InfluxOrg, m.config.InfluxBucket)
-
-	for _, peer := range peers {
-		p := influxdb2.NewPoint(
-			"peer_stats",
-			map[string]string{
-				"ip":     peer.IP,
-				"client": peer.Client,
-				"hash":   peer.Hash,
-				"name":   peer.Name,
-			},
-			map[string]interface{}{
-				"progress":   peer.Progress,
-				"downloaded": peer.Downloaded,
-				"uploaded":   peer.Uploaded,
-			},
-			time.Unix(0, peer.TimestampNS),
-		)
-
-		if err := writeAPI.WritePoint(context.Background(), p); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (m *SwarmMonitor) monitorTorrent(hash string) {
-	torrentInfo, err := m.getTorrentInfo(hash)
-	if err != nil {
-		log.Printf("Error getting torrent info for %s: %v", hash, err)
-		return
-	}
-
-	monitor := &TorrentMonitor{
-		startTime: time.Now(),
-		hash:      hash,
-		name:      torrentInfo.Name,
-		active:    true,
-	}
-
-	m.mu.Lock()
-	m.monitors[hash] = monitor
-	m.mu.Unlock()
-
-	for start := time.Now(); time.Since(start) < 30*time.Minute; {
-		monitor.mu.Lock()
-		if !monitor.active {
-			monitor.mu.Unlock()
-			return
-		}
-		monitor.mu.Unlock()
-
-		elapsed := time.Since(start)
-		var interval time.Duration
-
-		switch {
-		case elapsed < 10*time.Minute:
-			interval = time.Second
-		case elapsed < 30*time.Minute:
-			interval = 5 * time.Second
-		}
-
-		peers, err := m.getPeerInfo(hash, torrentInfo.Name)
-		if err != nil {
-			log.Printf("Error getting peer info for %s (%s): %v", torrentInfo.Name, hash, err)
-			time.Sleep(interval)
-			continue
-		}
-
-		if err := m.writePeerData(peers); err != nil {
-			log.Printf("Error writing peer data for %s (%s): %v", torrentInfo.Name, hash, err)
-		}
-
-		time.Sleep(interval)
-	}
-
-	monitor.mu.Lock()
-	monitor.active = false
-	monitor.mu.Unlock()
-}
-
-func (m *SwarmMonitor) periodicCheck() {
+func monitorTorrent(name, hash string, client *qbittorrent.Client, ctx context.Context, db *sql.DB) {
 	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
 
-	for range ticker.C {
-		m.mu.RLock()
-		hashes := make([]string, 0, len(m.monitors))
-		for hash, monitor := range m.monitors {
-			monitor.mu.Lock()
-			if !monitor.active {
-				hashes = append(hashes, hash)
-			}
-			monitor.mu.Unlock()
-		}
-		m.mu.RUnlock()
+	var torrentID int
+	err := db.QueryRow(`
+		INSERT INTO torrents (name, hash)
+		VALUES (?, ?)
+		ON CONFLICT(hash)
+		DO UPDATE SET name = excluded.name
+		RETURNING id
+	`, name, hash).Scan(&torrentID)
+	if err != nil {
+		log.Printf("Error inserting or getting torrent ID: %v", err)
+		return
+	}
 
-		for _, hash := range hashes {
-			torrentInfo, err := m.getTorrentInfo(hash)
+	// Send notification about the new torrent
+	select {
+	case newTorrentChan <- TorrentNotification{Hash: hash, ID: torrentID}:
+	default:
+		log.Printf("Warning: newTorrentChan is full, couldn't notify about new torrent %s", hash)
+	}
+
+	lastRid := 0
+	lastUploaded := make(map[int]int64)
+	lastDownloaded := make(map[int]int64)
+	for {
+		select {
+		case <-ticker.C:
+			peers, err := client.SyncTorrentPeers(hash, lastRid)
 			if err != nil {
-				log.Printf("Error getting torrent info for %s: %v", hash, err)
+				log.Printf("Error getting peer info for hash %s: %v", hash, err)
 				continue
 			}
+			lastRid = peers.Rid
 
-			peers, err := m.getPeerInfo(hash, torrentInfo.Name)
-			if err != nil {
-				log.Printf("Error getting peer info for %s (%s): %v", torrentInfo.Name, hash, err)
-				continue
-			}
-
-			if len(peers) > 0 {
-				if err := m.writePeerData(peers); err != nil {
-					log.Printf("Error writing peer data for %s (%s): %v", torrentInfo.Name, hash, err)
+			func() {
+				tx, err := db.Begin()
+				if err != nil {
+					log.Printf("Error starting transaction: %v", err)
+					return
 				}
-			}
+				defer func() {
+					if p := recover(); p != nil {
+						tx.Rollback()
+						panic(p)
+					} else if err != nil {
+						tx.Rollback()
+					} else {
+						err = tx.Commit()
+						if err != nil {
+							log.Printf("Error committing transaction: %v", err)
+						}
+					}
+				}()
+
+				for _, peer := range peers.Peers {
+					var peerID int
+					err := tx.QueryRow(`
+								INSERT INTO peers (ip, port, client)
+								VALUES (?, ?, ?)
+								ON CONFLICT(ip, port)
+								DO UPDATE SET client = excluded.client
+								RETURNING id
+							`, peer.IP, peer.Port, peer.Client).Scan(&peerID)
+					if err != nil {
+						log.Printf("Error inserting or getting peer ID: %v", err)
+						continue
+					}
+					ts := time.Now().UTC()
+					uploaded := peer.Uploaded - lastUploaded[peerID]
+					if uploaded < 0 {
+						uploaded = 0
+					}
+					downloaded := peer.Downloaded - lastDownloaded[peerID]
+					if downloaded < 0 {
+						downloaded = 0
+					}
+					_, err = tx.Exec(`INSERT INTO peer_metrics (timestamp, peer_id, torrent_id, uploaded, downloaded, pct_complete)
+					VALUES (?, ?, ?, ?, ?, ?)`,
+						ts.Format(time.RFC3339), peerID, torrentID, uploaded, downloaded, peer.Progress)
+					if err != nil {
+						log.Printf("Error inserting peer metrics: %v", err)
+						continue
+					}
+
+					fmt.Printf("%s peer_metrics %40s,%15s:%-5d: %20s %5.3f %12d %12d\n",
+						ts.Format(time.Stamp), hash, peer.IP, peer.Port, peer.Client, peer.Progress, downloaded, uploaded)
+					lastUploaded[peerID] = peer.Uploaded
+					lastDownloaded[peerID] = peer.Downloaded
+				}
+			}()
+		case <-ctx.Done():
+			log.Printf("Stopping monitor for hash: %s", hash)
+			return
 		}
 	}
 }
 
-func (m *SwarmMonitor) listenSocket() error {
-	if err := os.Remove(m.config.SocketPath); err != nil && !os.IsNotExist(err) {
-		return err
+func periodicCheck(client *qbittorrent.Client, ctx context.Context, db *sql.DB, newTorrentChan <-chan TorrentNotification) {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	rid := 0
+	mainData, err := client.SyncMainData(rid)
+	if err != nil {
+		log.Fatalf("Error getting initial main data: %v", err)
+	}
+	lastUploaded := make(map[string]int64)
+	lastDownloaded := make(map[string]int64)
+	hash_ids := make(map[string]int)
+
+	func() {
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatalf("Error starting transaction: %v", err)
+		}
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				if commitErr := tx.Commit(); commitErr != nil {
+					log.Printf("Error committing transaction: %v", commitErr)
+				}
+			}
+		}()
+
+		// Insert torrents into the torrents table and remember their ids
+		for hash, torrent := range mainData.Torrents {
+			var id int
+			err = tx.QueryRow(`INSERT INTO torrents (hash, name) VALUES (?, ?)
+				ON CONFLICT(hash) DO UPDATE SET name = excluded.name
+				RETURNING id`,
+				hash, torrent.Name).Scan(&id)
+			if err != nil {
+				log.Printf("Error inserting torrent %s: %v", hash, err)
+				continue
+			}
+			hash_ids[hash] = id
+			lastUploaded[hash] = torrent.Uploaded
+			lastDownloaded[hash] = torrent.Downloaded
+		}
+	}()
+
+	for {
+		select {
+		case <-ticker.C:
+			mainData, err := client.SyncMainData(rid)
+			if err != nil {
+				log.Printf("Error getting main data: %v", err)
+				continue
+			}
+			rid = mainData.Rid
+
+			func() {
+				tx, err := db.Begin()
+				if err != nil {
+					log.Printf("Error starting transaction: %v", err)
+					return
+				}
+				defer func() {
+					if p := recover(); p != nil {
+						tx.Rollback()
+						panic(p)
+					} else if err != nil {
+						tx.Rollback()
+					} else {
+						err = tx.Commit()
+						if err != nil {
+							log.Printf("Error committing transaction: %v", err)
+						}
+					}
+				}()
+
+				for hash, torrent := range mainData.Torrents {
+					if torrent.Uploaded > lastUploaded[hash] || torrent.Downloaded > lastDownloaded[hash] {
+						// Insert or update main metrics
+						uploaded := torrent.Uploaded - lastUploaded[hash]
+						if uploaded < 0 {
+							uploaded = 0
+						}
+						downloaded := torrent.Downloaded - lastDownloaded[hash]
+						if downloaded < 0 {
+							downloaded = 0
+						}
+						ts := time.Now().UTC()
+						_, err := tx.Exec(`INSERT INTO main_metrics (timestamp, torrent_id, uploaded, downloaded)
+							VALUES (?, ?, ?, ?)`,
+							ts.Format(time.RFC3339), hash_ids[hash], uploaded, downloaded)
+						if err != nil {
+							log.Printf("Error inserting main metrics (%s): %v", hash, err)
+							continue
+						}
+						fmt.Printf("%s main_metrics %40s: %12d %12d\n", ts.Format(time.Stamp), hash, uploaded, downloaded)
+
+						lastUploaded[hash] = torrent.Uploaded
+						lastDownloaded[hash] = torrent.Downloaded
+					}
+				}
+			}()
+
+		case notification := <-newTorrentChan:
+			hash_ids[notification.Hash] = notification.ID
+			log.Printf("Added new torrent to periodic check: %s (ID: %d)", notification.Hash, notification.ID)
+
+		case done := <-ctx.Done():
+			log.Printf("Context done: %v", done)
+			return
+		}
+	}
+}
+
+func run(cmd *cobra.Command, args []string) {
+	loadConfig(configPath) // Override defaults with config file values
+
+	qbClient, err := qbittorrent.NewClient(qBitUsername, qBitPassword, qBitAddr, qBitPort)
+	if err != nil {
+		log.Fatalf("Failed to create qBittorrent client: %v", err)
 	}
 
-	listener, err := net.Listen("unix", m.config.SocketPath)
+	db := initDB()
+
+	// Context to manage goroutines
+	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(fmt.Errorf("deferred cancel"))
+
+	newTorrentChan = make(chan TorrentNotification, 10) // adjust buffer size as needed
+
+	go periodicCheck(qbClient, ctx, db, newTorrentChan)
+
+	if err := os.Remove(socketPath); err != nil && !os.IsNotExist(err) {
+		log.Fatalf("Failed to remove existing socket: %v", err)
+	}
+
+	listener, err := net.Listen("unix", socketPath)
 	if err != nil {
-		return err
+		log.Fatalf("Failed to listen on socket: %v", err)
 	}
 	defer listener.Close()
 
-	go m.periodicCheck()
+	log.Printf("Listening on socket: %s", socketPath)
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Accept error: %v", err)
-			continue
+	// Handle interrupt signals for graceful shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	// Goroutine to accept connections, main code waits for interrupts
+	go func() {
+		for {
+			select {
+			case done := <-ctx.Done():
+				log.Printf("Context cancelled: %v", done)
+				return
+			default:
+				conn, err := listener.Accept()
+				if err != nil {
+					if netError, ok := err.(net.Error); ok && netError.Timeout() {
+						continue
+					}
+					log.Printf("Accept error: %v", err)
+					break
+				}
+				handleConnection(ctx, conn, qbClient, db)
+			}
 		}
+	}()
 
-		var hash string
-		if err := json.NewDecoder(conn).Decode(&hash); err != nil {
-			log.Printf("Decode error: %v", err)
-			conn.Close()
-			continue
+	<-interrupt
+	log.Println("Received interrupt signal. Shutting down...")
+	cancel(fmt.Errorf("received interrupt signal"))
+}
+
+func handleConnection(ctx context.Context, conn net.Conn, qbClient *qbittorrent.Client, db *sql.DB) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Printf("Read error: %v", err)
+		} else {
+			log.Println("Connection closed before receiving input")
 		}
-		conn.Close()
-
-		go m.monitorTorrent(hash)
+		return
 	}
+
+	input := scanner.Text()
+	fields := strings.Fields(input)
+	if len(fields) != 2 {
+		log.Println("Expected name and hash separated by whitespace. Ignoring.")
+		return
+	}
+
+	// we assume Fields can't ever return empty strings
+	name := fields[0]
+	hash := fields[1]
+	log.Printf("Received name: %s, hash: %s", name, hash)
+
+	go monitorTorrent(name, hash, qbClient, ctx, db)
 }
 
 func main() {
-	socketPath := flag.String("socket", "/tmp/torrent-monitor.sock", "Unix socket path")
-	flag.Parse()
-
-	config := Config{
-		QBitHost:     "http://localhost:8080",
-		QBitUsername: "admin",
-		QBitPassword: "adminpass",
-		InfluxHost:   "http://localhost:8086",
-		InfluxToken:  "your-token",
-		InfluxOrg:    "your-org",
-		InfluxBucket: "torrent-metrics",
-		SocketPath:   *socketPath,
+	rootCmd = &cobra.Command{
+		Use:   "swarmmonitor",
+		Short: "SwarmMonitor is a tool to monitor torrent swarms",
+		Run:   run,
 	}
 
-	monitor, err := NewSwarmMonitor(config)
-	if err != nil {
-		log.Fatal(err)
+	rootCmd.Flags().StringVarP(&configPath, "config", "c", configPath, "Path to config file")
+	rootCmd.Flags().StringVar(&socketPath, "socket", "", "Unix socket path")
+	rootCmd.Flags().StringVar(&qBitAddr, "qbit-addr", "", "qBittorrent host")
+	rootCmd.Flags().StringVar(&qBitPort, "qbit-port", "", "qBittorrent webui port")
+	rootCmd.Flags().StringVar(&qBitUsername, "qbit-username", "", "qBittorrent username")
+	rootCmd.Flags().StringVar(&qBitPassword, "qbit-password", "", "qBittorrent password")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Printf("Error executing rootCmd: %v", err)
+		os.Exit(1)
+	}
+}
+
+func loadConfig(configPath string) {
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("No config file found or error reading config file: %v\n", err)
+		return
+	}
+	log.Printf("Using config file: %s\n", viper.ConfigFileUsed())
+
+	flags := map[string]*string{
+		"socket":        &socketPath,
+		"qbit-addr":     &qBitAddr,
+		"qbit-port":     &qBitPort,
+		"qbit-username": &qBitUsername,
+		"qbit-password": &qBitPassword,
 	}
 
-	if err := monitor.login(); err != nil {
-		log.Fatal("Login failed:", err)
-	}
-
-	log.Printf("Listening on socket: %s", config.SocketPath)
-	if err := monitor.listenSocket(); err != nil {
-		log.Fatal(err)
+	for flag, ptr := range flags {
+		if rootCmd.Flags().Changed(flag) {
+			continue
+		}
+		if v := viper.GetString(flag); v != "" {
+			*ptr = v
+		}
 	}
 }
